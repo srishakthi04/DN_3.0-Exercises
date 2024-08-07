bf04118ea5f9e694346b6226f5a47f9922a6cc91
SRISHAKTHI S R <132913726+srishakthi04@users.noreply.github.com>
2024-08-07 12:12:06 +0530

    Add files via upload

---
+++ week2/spring/librarymanagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void displayRepositoryInfo() {
+        System.out.println("BookRepository is successfully injected into BookService.");
+    }
+    
+}
