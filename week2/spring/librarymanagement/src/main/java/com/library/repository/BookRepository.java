473fa05dafad0c7fb4cfbab36745c6ab01e87712
SRISHAKTHI S R <132913726+srishakthi04@users.noreply.github.com>
2024-08-10 20:58:35 +0530

    Delete week2/spring/librarymanagement directory

--- week2/spring/librarymanagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void displayRepositoryInfo() {
-        System.out.println("BookRepository is successfully injected into BookService.");
-    }
-    
-}
